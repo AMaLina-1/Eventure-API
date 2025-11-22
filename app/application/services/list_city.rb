51941240e25218c387b13e3bff5641bb6ffdd2bd
gelicheng <geli@LAPTOP-BL2BC8TS.>
2025-11-22 16:18:10 +0800

    add city list service object

---
+++ app/application/services/list_city.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'dry/transaction'
+
+module Eventure
+  module Service
+    # Service to list cities
+    class ListCity
+      include Dry::Transaction
+
+      step :fetch_cities
+
+      DB_ERR = 'Cannot access database'
+
+      private
+
+      def fetch_cities(input)
+        activities = Repository::For.klass(Entity::Activity).all
+        cities = activities.map(&:city).uniq
+        list = Eventure::Response::CityList.new(cities)
+        Response::ApiResult.new(status: :ok, message: list)
+          .then { |result| Success(result) }
+      rescue StandardError
+        Failure(Response::ApiResult.new(status: :internal_error, message: DB_ERR))
+      end
+    end
+  end
+end

4388bee1e38ab488dcea6a95af31642c60c4d4ae
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2025-12-06 19:06:26 +0800

    delete: tag_id

---
+++ db/migrations/009_tags_remove_tag_id_add_unique.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+Sequel.migration do
+  up do
+    # SQLite 需要重建整個表來修改主鍵
+    create_table(:tags_new) do
+      primary_key :id
+      String :tag, null: true, unique: true
+    end
+
+    # 複製資料（如果有的話）
+    run 'INSERT INTO tags_new (tag) SELECT DISTINCT tag FROM tags WHERE tag IS NOT NULL'
+
+    # 刪除舊表
+    drop_table(:tags)
+
+    # 重命名新表
+    rename_table(:tags_new, :tags)
+
+    # 重建關聯表
+    drop_table(:activities_tags) if table_exists?(:activities_tags)
+    create_table(:activities_tags) do
+      foreign_key :activity_id, :activities, on_delete: :cascade
+      foreign_key :tag_id, :tags, on_delete: :cascade
+      primary_key %i[activity_id tag_id]
+    end
+  end
+
+  down do
+    drop_table(:activities_tags)
+    create_table(:tags) do
+      Integer :tag_id, primary_key: true
+      String :tag, null: true
+    end
+    create_table(:activities_tags) do
+      foreign_key :activity_id, :activities, on_delete: :cascade
+      foreign_key :tag_id, :tags, on_delete: :cascade
+      primary_key %i[activity_id tag_id]
+    end
+  end
+end
